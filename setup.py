ff48528a90d144dfc9710ad22049292bd9d0794b
littletiger7 <little_tiger7@163.com>
2019-07-09 18:22:32 +0800

    0.0.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sparrow_django_common',
+    version='0.0.1',
+    author="hg",
+    description='Including a variety of common middleware',
+    license='MIT License',
+    url='https://github.com/hanguangbaihuo/sparrow_django_common.git',
+    packages=find_packages(),
+)
